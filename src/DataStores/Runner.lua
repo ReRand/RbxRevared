0a07965a7e4c92e038a5bf6db38b652bf1178c4f
paige <88659700+paishee@users.noreply.github.com>
2024-03-28 09:15:40 -0500

    Create Runner.lua

---
+++ src/DataStores/Runner.lua
@@ -0,0 +1,28 @@
+local DataStoreService = game:GetService("DataStoreService");
+
+
+local DataStores = require(script.Parent);
+local events = script.Parent.Events;
+
+
+local request = events.Request;
+local post = events.Post;
+local out = events.Out;
+
+
+request.OnServerEvent:Connect(function(player, dataStore, scope, key)
+    local store = DataStoreService:GetDataStore(dataStore, scope);
+
+    local data = store:GetAsync(tostring(key));
+
+    out:FireClient(player, DataStores.Types.Request, data);
+end);
+
+
+post.OnServerEvent:Connect(function(player, dataStore, scope, key, data)
+    local store = DataStoreService:GetDataStore(dataStore, scope);
+
+    store:SetAsync(tostring(key), data);
+
+    out:FireClient(player, DataStores.Types.Post, data);
+end);
