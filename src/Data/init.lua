76720b0ce6a449742d7be0558936e76c5beff451
paige <88659700+paishee@users.noreply.github.com>
2024-03-28 08:53:22 -0500

    Create init.lua

---
+++ src/Data/init.lua
@@ -0,0 +1,72 @@
+local Signal = require(script.Parent.Signal);
+
+
+local Data = {
+    Types = {
+        Request = 1,
+        Post = 2,
+    },
+
+    Requested = Signal.new(),
+    Posted = Signal.new()
+};
+
+
+local request = script.Events.Request;
+local post = script.Events.Post;
+local out = script.Events.Out;
+
+
+function Data:Request(dataStore: string, keyOrScope: string, key)
+    local scope = nil;
+
+    if keyOrScope and not key then
+        key = keyOrScope;
+    else
+        scope = keyOrScope;
+    end
+
+
+    request:FireServer(dataStore, scope, key);
+
+
+    while task.wait() do
+        local reqType, data = out.OnClientEvent:Wait();
+
+        if reqType == Data.Types.Request then
+            Data.Requested:Fire(data);
+            return data;
+        end
+    end
+end
+
+
+function Data:Post(dataStore: string, keyOrScope: string, keyOrData, data)
+    local scope = nil;
+    local key = nil;
+
+
+    if keyOrScope and not data then
+        key = keyOrScope;
+        data = keyOrData;
+    else
+        scope = keyOrScope;
+        key = keyOrData;
+    end
+
+
+    post:FireServer(dataStore, scope, key, data);
+
+
+    while task.wait() do
+        local reqType, data = out.OnClientEvent:Wait();
+
+        if reqType == Data.Types.Post then
+            Data.Posted:Fire(data);
+            return data;
+        end
+    end
+end
+
+
+return Data;
