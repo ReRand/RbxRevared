b76c4889c629213d98f39764ab085d1d524f749a
paige <88659700+paishee@users.noreply.github.com>
2024-03-29 12:12:05 -0500

    Create ServerSide.server.lua

---
+++ Revared/Services/GlobalSide/Functions/Damage/ServerSide.server.lua
@@ -0,0 +1,38 @@
+-- events
+local clientEv = script.Parent.PingPong.Client;
+local serverEv = script.Parent.PingPong.Server;
+local referServerEv = script.Parent.PingPong.ReferServer;
+
+
+local GlobalSide = require(script.Parent.Parent.Parent.Parent);
+
+
+function dmgInit(victim, amount)
+	local victimHuman = victim:WaitForChild("Humanoid");
+
+    local old = victimHuman.Health
+    
+	if (victimHuman.Health - amount < 0) then
+        amount = amount - math.abs(victimHuman.Health - amount))
+    end
+    
+	victimHuman.Health -= amount;
+
+	local res = {
+			Vicitm = victim, 
+			Amount = amount,
+			NewHealth = new,
+			OldHealth = old
+	};
+	
+    GlobalSide.Healed.Server:Fire(res);
+	clientEv:FireAllClients(victim, victimHuman.Health, old);
+end
+
+
+serverEv:Connect(function(player, victim, amount)
+	dmgInit(victim, amount);		
+end)
+
+
+referServerEv.Event:Connect(dmgInit);
