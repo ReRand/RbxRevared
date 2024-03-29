81ed60f7c9146d0fbfcde21a7d52f5abfb5218c9
paige <88659700+paishee@users.noreply.github.com>
2024-03-29 10:14:36 -0500

    Create Runner.server.lua

---
+++ Revared/Services/LocalBadges/Runner.server.lua
@@ -0,0 +1,24 @@
+local BadgeService = game:GetService("BadgeService");
+
+local main = script.Parent.Parent.Parent;
+local LocalBadges = require(main);
+local events = main.Events;
+
+
+local award = events.Award;
+local has = events.Has;
+local out = events.Out;
+
+
+award.OnServerEvent:Connect(function(initPlayer, badgeId, receiver)
+    BadgeService:AwardBadge(receiver.UserId, badgeId);
+
+    out:FireClient(player, LocalBadges.Types.Award, receiver);
+end);
+
+
+has.OnServerEvent:Connect(function(initPlayer, badgeId, player)
+    local doesHave = BadgeService:UserHasBadgeAsync(player.UserId, badgeId);
+
+    out:FireClient(player, LocalBadges.Types.Has, doesHave);
+end);
