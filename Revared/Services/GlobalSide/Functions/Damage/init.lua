41b27b059de6e6582767e3fdb03470a7da4e7273
paige <88659700+paishee@users.noreply.github.com>
2024-03-29 12:05:43 -0500

    Create init.lua

---
+++ Revared/Services/GlobalSide/Functions/Damage/init.lua
@@ -0,0 +1,30 @@
+-- events
+local clientEv = script.PingPong.Client;
+local serverEv = script.PingPong.Server;
+local referServerEv = script.PingPong.ReferServer;
+
+
+-- run context
+local RunService = game:GetService("RunService");
+
+local client = RunService:IsClient();
+local server = RunService:IsServer();
+
+
+-- stuff
+return (function(GlobalSide)
+
+        
+	function GlobalSide:Damage(victim, amount)
+		if victim:IsA("Humanoid") then victim = victim.Parent; end
+		elseif victim:IsA("Player") then victim = victim.Character or victim.CharacterAdded:Wait(); end
+			
+	    if server then
+            referServerEv:Fire(victim, amount);
+        elseif client then
+            serverEv:FireServer(victim, amount);
+        end
+	end
+
+        
+end)
