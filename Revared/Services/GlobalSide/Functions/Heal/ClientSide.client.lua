d1e19268b5eedef6f8351cf0c3ecd06d8a1fb69c
paige <88659700+paishee@users.noreply.github.com>
2024-03-29 12:04:01 -0500

    Update ClientSide.client.lua

--- Revared/Services/GlobalSide/Functions/Heal/ClientSide.client.lua
+++ Revared/Services/GlobalSide/Functions/Heal/ClientSide.client.lua
@@ -6,13 +6,20 @@ local GlobalSide = require(script.Parent.Parent.Parent.Parent);
 local player = game.Players.LocalPlayer;
 
 
-function healInit(victim, new, old)
+function healInit(victim, amount new, old)
 	local victimHuman = victim:WaitForChild("Humanoid");
     
-    
 	victimHuman.Health = new;
-    GlobalSide.Healed.Client:Fire(player, victim, new, old);
-    GlobalSide.Healed.Finally:Fire(victim, new, old);
+
+	local res = {
+			Vicitm = victim, 
+			Amount = amount,
+			NewHealth = new,
+			OldHealth = old
+	};
+	
+    GlobalSide.Healed.Client:Fire(player, res);
+    GlobalSide.Healed.Finally:Fire(res);
 end
 
 
