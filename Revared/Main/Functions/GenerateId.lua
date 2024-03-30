02ca6e236fd5a26170a7f0d6b840fef52285aabb
paige <88659700+paishee@users.noreply.github.com>
2024-03-30 13:53:34 -0500

    Update GenerateId.lua

--- Revared/Main/Functions/GenerateId.lua
+++ Revared/Main/Functions/GenerateId.lua
@@ -1,36 +1,34 @@
-local function split(str, sep)
-    if sep == nil then sep = "%s" end
-    local spl = {};
-
-    for s in string.gmatch(str, "([^"..sep.."]+)") do
-        table.insert(spl, s);
-    end
-
-    return spl;
+local function split(str)
+	local list = {};
+	
+	str:gsub(".", function(char) 
+		table.insert(list, char) 
+	end)
+	
+	return list;
 end
-
 
 return (function(Revared)
 
-    
-    function Revared:GenerateId(length, characters)
-		if not length then length = 10 end;
-        local id = {};
 
-        if characters then
-            if type(characters) == "string" then
-                characters = split(characters);
-            end
-        else
-            characters = split("1234567890");
-        end
-    
-        for i=1, length do
-            id[i] = characters[math.random(1, #characters)];
-        end
+	function Revared:GenerateId(length, characters)
+		if not length then length = 10 end;
+		local id = {};
+
+		if not characters then
+			characters = "1234567890";
+		end
+		
+		characters = split(characters);
+		
+		print(characters);
+
+		for i=1, length do
+			id[i] = characters[math.random(1, #characters)];
+		end
 
 		return tostring(table.concat(id));
-    end
+	end
 
-    
+
 end)
