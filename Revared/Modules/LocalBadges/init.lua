efa6196fd52f817b55d5a8e930887462890bac5f
paige <88659700+paishee@users.noreply.github.com>
2024-03-30 13:08:29 -0500

    Update init.lua

--- Revared/Modules/LocalBadges/init.lua
+++ Revared/Modules/LocalBadges/init.lua
@@ -18,7 +18,7 @@ local out = script.Events.Out;
 
 
 function LocalBadges:Award(badgeId, player)
-    if not player then player = game.Players.LocalPlayer;
+    if not player then player = game.Players.LocalPlayer; end
     award:FireServer(badgeId, player);
 
 
@@ -34,7 +34,7 @@ end
 
 
 function LocalBadges:Has(badgeId, player)
-    if not player then player = game.Players.LocalPlayer;
+    if not player then player = game.Players.LocalPlayer; end
     has:FireServer(badgeId, player);
 
 
@@ -46,7 +46,7 @@ function LocalBadges:Has(badgeId, player)
             return data;
         end
     end
-  end
+end
 
 
 
