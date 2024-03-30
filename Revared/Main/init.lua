725b54f120c2d51cf282c0a262ac8c087cf17498
paige <88659700+paishee@users.noreply.github.com>
2024-03-30 13:57:22 -0500

    Update init.lua

--- Revared/Main/init.lua
+++ Revared/Main/init.lua
@@ -1,8 +1,8 @@
 --[[
 	== Revared: ReRand Game ToolKit ==
-	Created by Paige "paishee" Sheffield
+	Created by Paige Sheffield
 
-	https://gitub.com/ReRand/RbxRevared
+	https://gitub.com/paishee/RbxRevared
 ]]
 
 
@@ -12,52 +12,44 @@ local Revared = {
 };
 
 
-
-local modules = script:FindFirstChild("Modules");
-local experiments = script:FindFirstChild("Experiments");
-local functions = script:FindFirstChild("Functions");
-
+local modules = script.Modules;
+local experiments = script.Experiments;
+local functions = script.Functions;
 
 
-if modules then
-	for _, mod in pairs(modules:GetChildren()) do
-	    if mod:IsA("ModuleScript") then
-			local module = require(mod);
-			
-			if module["Init"] then
-				module:Init(Revared);
-			end
-			
-	        Revared[mod.Name] = module;
-			Revared.Modules[mod.Name] = module;
-	    end
-	end
+for _, mod in pairs(modules:GetChildren()) do
+    if mod:IsA("ModuleScript") then
+
+		local module = require(mod);
+		
+		if module["Init"] then
+			module:Init(Revared);
+		end
+		
+        Revared[mod.Name] = module;
+		Revared.Modules[mod.Name] = module;
+    end
 end
 
 
+for _, exp in pairs(experiments:GetChildren()) do
+    if exp:IsA("ModuleScript") then
 
-if experiments then
-	for _, exp in pairs(experiments:GetChildren()) do
-	    if exp:IsA("ModuleScript") then
-			local experiment = require(exp);
-			
-			if experiment["Init"] then
-				experiment:Init(Revared);
-			end
-			
-			Revared.Experiments[exp.Name] = experiment;
-	    end
-	end
+		local experiment = require(exp);
+		
+		if experiment["Init"] then
+			experiment:Init(Revared);
+		end
+		
+		Revared.Experiments[exp.Name] = experiment;
+    end
 end
 
 
-
-if functions then
-	for _, f in ipairs(functions:GetChildren()) do
-		require(f)(Revared);
-	end
+for _, f in ipairs(functions:GetChildren()) do
+	require(f)(Revared);
 end
 
 
-
+_G.Revared = Revared;
 return Revared;
