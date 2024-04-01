a9aa61b5fd0b08ca342c611b0549aad09320c009
paige <88659700+paishee@users.noreply.github.com>
2024-04-01 11:32:58 -0500

    Update GetMoveDirection.lua

--- Revared/Modules/PlayerControls/Functions/GetMoveDirection.lua
+++ Revared/Modules/PlayerControls/Functions/GetMoveDirection.lua
@@ -1,10 +1,19 @@
+Enum["MoveDirection"] = {};
+
+Enum["MoveDirection"]["Left"] = 1;
+
+
 return (function(PlayerControls)
 
+		
     function PlayerControls:GetMoveDirection(player)
         if player:IsA("Humanoid") then player = player.Parent;
 		elseif player:IsA("Player") then player = player.Character or player.CharacterAdded:Wait(); end
 
+		local moveDir = nil;
+
         
     end
-        
+
+		
 end)
