73bf2f28b090d28db88ccd7d86907180d28c8ddf
shysolocup <88659700+shysolocup@users.noreply.github.com>
2024-04-23 09:24:26 -0500

    Create new.lua

---
+++ Revared/Modules/Dict/Entry/Functions/new.lua
@@ -0,0 +1,24 @@
+local Revared = _G.Revared;
+local Signal = Revared:GetModule("Signal");
+
+
+return (function(Entry, Dict)
+
+    function Entry.new(dict, key, index, value, type)
+    	local self = setmetatable( {
+            Key = key,
+            Index = index,
+            Value = value,
+            Type = type,
+            
+            Changed = Signal.new(),
+            Destroyed = Signal.new(),
+    
+            Parent = dict
+        }, Dict );
+    
+    	return self;
+    end
+
+        
+end)
