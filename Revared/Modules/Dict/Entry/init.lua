e3f95266c80b1e4329d0e26e7d245f64bca1ee98
shysolocup <88659700+shysolocup@users.noreply.github.com>
2024-04-23 09:18:52 -0500

    Create init.lua

---
+++ Revared/Modules/Dict/Entry/init.lua
@@ -0,0 +1,16 @@
+local Entry = {}
+Entry.__index = Entry;
+
+
+local functions = script.Functions;
+local main = _G.Revared;
+
+
+function Entry.init(Dict)
+  for _, f in ipairs(functions:GetChildren()) do
+  	require(f)(Entry, Dict);
+  end
+end
+
+
+return Entry;
