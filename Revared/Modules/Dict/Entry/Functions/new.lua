1cf900413b580aeb9a8e9bdba5efa19b1df5c0a8
shysolocup <88659700+shysolocup@users.noreply.github.com>
2024-04-23 12:16:51 -0500

    Update new.lua

--- Revared/Modules/Dict/Entry/Functions/new.lua
+++ Revared/Modules/Dict/Entry/Functions/new.lua
@@ -4,19 +4,41 @@ local Signal = Revared:GetModule("Signal");
 
 return (function(Entry, Dict)
 
-    function Entry.new(dict, key, index, value, type)
+    function Entry.new(dict, key, value, type)
     	local self = setmetatable( {
             Key = key,
             Index = nil,
             Value = value,
             Type = type,
+            Id = nil,
             
             Changed = Signal.new(),
             Destroyed = Signal.new(),
     
             Parent = dict
         }, Dict );
-    
+
+            
+        self.Id = tostring(self):sub(type(self):len() + 3);
+
+            
+        Entry.__index = function(table, key)
+            if key == "Index" then
+                for i, entry in ipairs(table.Parent.__dictdata) do
+                    if entry.Id == table.Id then
+                        return i;
+                    end
+                end
+                    
+            elseif rawget(table, ki) then
+                return table[ki];
+                    
+            else
+                return nil;
+            end    
+        end
+
+            
     	return self;
     end
 
