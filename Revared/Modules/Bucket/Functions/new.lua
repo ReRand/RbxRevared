60dcc3d34a7160647d27791ae4066b07a2d3150d
shysolocup <88659700+shysolocup@users.noreply.github.com>
2024-04-23 08:25:22 -0500

    Create new.lua

---
+++ Revared/Modules/Bucket/Functions/new.lua
@@ -0,0 +1,51 @@
+return (function(Bucket)
+
+    function Bucket.new(array)
+
+    	local self = setmetatable( {}, Bucket )
+    	local length = 0
+
+            
+    	if array then
+    		for i, v in pairs(array) do
+    			local stuff
+    			local vl = 0
+    			local str = tostring(v)
+    
+    			if type(v) == "table" then for _ in pairs(v) do vl = vl + 1 end end
+    
+    			if string.sub(str,1,string.len("(("))=="((" then
+    				stuff = {
+    					Key = i,
+    					Value = v,
+    					Type = Bucket.Types.Uni
+    				}
+    
+    			elseif type(v) == "table" and vl == 1 then
+    				for Key, Value in pairs(v) do
+    					stuff = {
+    						Key = Key,
+    						Value = Value,
+    						Type = Bucket.Types.Pair
+    					}
+    					break
+    				end
+    			else
+    				stuff = {
+    					Key = i,
+    					Value = v,
+    					Type = Bucket.Types.Uni
+    				}
+    			end
+    
+    			self[length+1] = stuff
+    
+    			length = length + 1
+    		end
+    	end
+    
+    	return self
+    end
+
+        
+end)
