d6b34e7a45ed93dd299054ea13c3b0c10c561bf4
shysolocup <88659700+shysolocup@users.noreply.github.com>
2024-04-23 15:12:28 -0500

    Create Clone.lua

---
+++ Revared/Modules/Dict/Functions/Clone.lua
@@ -0,0 +1,13 @@
+-- wip
+return (function(Dict)
+
+    function Dict:Clone()
+        local cloned = Dict.new();
+        
+            
+        --[[for key, entry in pairs(self.__dictdata) do
+            cloned.__dictdata[key] = entry;
+        end]]
+    end
+    
+end)
