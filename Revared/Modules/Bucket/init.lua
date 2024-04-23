0a252cd4129ccc0022e27acb9f7b6b14f2172b98
shysolocup <88659700+shysolocup@users.noreply.github.com>
2024-04-23 08:23:27 -0500

    Create init.lua

---
+++ Revared/Modules/Bucket/init.lua
@@ -0,0 +1,14 @@
+local Bucket = {}
+Bucket.__index = Bucket
+
+
+local functions = script.Functions;
+local main = _G.Revared;
+
+
+for _, f in ipairs(functions:GetChildren()) do
+	require(f)(Bucket);
+end
+
+
+return Bucket;
