83283711ed1643f23ea4dbf2578e2de253d11c44
shysolocup <88659700+shysolocup@users.noreply.github.com>
2024-04-24 09:18:44 -0500

    Update __tostring.lua

--- Revared/Modules/Dict/Functions/__tostring.lua
+++ Revared/Modules/Dict/Functions/__tostring.lua
@@ -1,15 +1,33 @@
--- https://stackoverflow.com/questions/9168058/how-to-dump-a-table-to-console
+-- help from https://stackoverflow.com/questions/9168058/how-to-dump-a-table-to-console
 
-function dump(o)
+function stringify(o, indents, layer)
+    if not layer then layer = 0; end
+    
+    local tab = ""
+    
+    for i = 1, indents do 
+      tab = tab.." ";
+    end
+    
+    for i = 1, layer do
+      tab = tab..tab;
+    end
+    
+    print('"'..tab..'"');
+    
    if type(o) == 'table' then
       local s = '{\n'
       for k,v in pairs(o) do
-         if type(k) ~= 'number' then k = '"'..k..'"' end
-         s = s .. k ..': ' .. dump(v) .. ',\n'
+         if layer == 0 then
+           s = s..stringify(v, indents, layer+1) .. ','
+          else
+            s = s .. k ..': ' .. stringify(v, indents, layer+1) .. ',\n'
+          end
       end
-      return s .. '\n}'
+      return s .. '}'
    else
-      return tostring(o)
+      if type(o) == "string" then return '"'..o..'"';
+      else return tostring(o); end
    end
 end
 
