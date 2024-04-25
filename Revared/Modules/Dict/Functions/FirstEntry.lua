37b1d9720ed0437a1a9ceda451a2a9384172507b
shysolocup <88659700+shysolocup@users.noreply.github.com>
2024-04-25 08:27:08 -0500

    Update FirstEntry.lua

--- Revared/Modules/Dict/Functions/FirstEntry.lua
+++ Revared/Modules/Dict/Functions/FirstEntry.lua
@@ -2,7 +2,7 @@ return (function(Dict)
 
     function Dict:FirstEntry(offset)
       if not offset then offset = 0; end
-      return self[1+offset];
+      return self.__dictdata[1+offset];
     end
     
   end)
