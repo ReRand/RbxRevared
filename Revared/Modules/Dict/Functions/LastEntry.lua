5d18b076f75a29ec5cf7193d50ffe1a7f72c4ffe
shysolocup <88659700+shysolocup@users.noreply.github.com>
2024-04-25 08:29:40 -0500

    Update LastEntry.lua

--- Revared/Modules/Dict/Functions/LastEntry.lua
+++ Revared/Modules/Dict/Functions/LastEntry.lua
@@ -2,7 +2,7 @@ return (function(Dict)
 
     function Dict:LastEntry(offset)
       if not offset then offset = 0; end
-      return self[self.Length-offset];
+      return self.__dictdata[self.Length-offset];
     end
     
   end)
