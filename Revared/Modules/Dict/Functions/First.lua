72684065f42461fefb682bd4f06996b469567077
shysolocup <88659700+shysolocup@users.noreply.github.com>
2024-04-25 08:26:47 -0500

    Update First.lua

--- Revared/Modules/Dict/Functions/First.lua
+++ Revared/Modules/Dict/Functions/First.lua
@@ -2,7 +2,7 @@ return (function(Dict)
 
     function Dict:First(offset)
       if not offset then offset = 0; end
-      return self[1+offset].Value;
+      return self.__dictdata[1+offset].Value;
     end
     
   end)
