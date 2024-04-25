7dca5a45aed1020b3359e0c75131cb58b212666a
shysolocup <88659700+shysolocup@users.noreply.github.com>
2024-04-25 08:26:25 -0500

    Update Last.lua

--- Revared/Modules/Dict/Functions/Last.lua
+++ Revared/Modules/Dict/Functions/Last.lua
@@ -2,7 +2,7 @@ return (function(Dict)
 
     function Dict:Last(offset)
       if not offset then offset = 0; end
-      return self[self.Length-offset].Value;
+      return self.__dictdata[self.Length-offset].Value;
     end
     
   end)
