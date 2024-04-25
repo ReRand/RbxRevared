38eb578dddd03befd75d4ed0d0714de2f6f7a103
shysolocup <88659700+shysolocup@users.noreply.github.com>
2024-04-25 08:27:22 -0500

    Update FirstKey.lua

--- Revared/Modules/Dict/Functions/FirstKey.lua
+++ Revared/Modules/Dict/Functions/FirstKey.lua
@@ -2,7 +2,7 @@ return (function(Dict)
 
     function Dict:FirstKey(offset)
       if not offset then offset = 0; end
-      return self[1+offset].Key;
+      return self.__dictdata[1+offset].Key;
     end
     
   end)
