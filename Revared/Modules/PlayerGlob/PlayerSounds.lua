8da242d9043547cc46223c2909ef51232c25372b
shysolocup <88659700+shysolocup@users.noreply.github.com>
2024-06-14 01:14:07 -0500

    Create PlayerSounds.lua

---
+++ Revared/Modules/PlayerGlob/PlayerSounds.lua
@@ -0,0 +1,12 @@
+local PlayerSounds = {}
+local players = game.Players;
+
+local attached = {};
+
+function PlayerSounds:Attach(sounds)
+	for _, sound in pairs(sounds) do
+		table.insert()
+	end
+end
+
+return PlayerSounds
