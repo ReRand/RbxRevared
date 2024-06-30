c6e50b0579f4010e39d3fd71ae5b97f7f47a9803
shysolocup <88659700+shysolocup@users.noreply.github.com>
2024-06-30 11:54:33 -0500

    Update TouchParams.lua

--- Revared/Modules/TouchGenius/TouchParams.lua
+++ Revared/Modules/TouchGenius/TouchParams.lua
@@ -1,30 +1,45 @@
-local TouchParams = {};
-TouchParams.__index = TouchParams;
+return (function(TouchGenius)
+	
+	local TouchParams = {};
+	TouchParams.__index = TouchParams;
 
 
-
-function TouchParams.new(paramTable)
-	if not paramTable then paramTable = {} end;
-	
-	if not paramTable.Maintain then paramTable.Maintain = true; end
-	if not paramTable.MaintainLoopDelay then paramTable.MaintainLoopDelay = 0 end;
-	if not paramTable.Filter or (paramTable.Filter and typeof(paramTable) ~= "table") then paramTable.Filter = {}; end
-	if not paramTable.RawResults then paramTable.RawResults = false end;
-	
-	
-	local self = setmetatable({
-		Maintain = paramTable.Maintain,
-		MaintainLoopDelay = paramTable.MaintainLoopDelay,
-		Filter = paramTable.Filter,
-		RawResults = paramTable.RawResults,
+	function TouchParams.new(paramTable)
+		if not paramTable then paramTable = {} end;
 		
-		ParamTable = paramTable
+		if not paramTable.Maintain then paramTable.Maintain = true; end
+		if not paramTable.MaintainLoopDelay then paramTable.MaintainLoopDelay = 0 end;
 		
-	}, TouchParams);
+		if not paramTable.MaintainFilter or (paramTable.MaintainFilter and typeof(paramTable.MaintainFilter) ~= "table") then paramTable.MaintainFilter = {}; end
+		if not paramTable.MaintainFilterType then paramTable.MaintainFilterType = TouchGenius.TouchFilterTypes.Exclude end
+		
+		if not paramTable.TouchFilter or (paramTable.TouchFilter and typeof(paramTable.TouchFilter) ~= "table") then paramTable.TouchFilter = {}; end
+		if not paramTable.TouchFilterType then paramTable.TouchFilterType = TouchGenius.TouchFilterTypes.Exclude end
+		
+		if not paramTable.RawResults then paramTable.RawResults = false end;
+		
+		
+		local self = setmetatable({
+			Maintain = paramTable.Maintain,
+			MaintainLoopDelay = paramTable.MaintainLoopDelay,
+			
+			MaintainFilter = paramTable.MaintainFilter,
+			MaintainFilterType = paramTable.MaintainFilterType,
+			
+			TouchFilter = paramTable.TouchFilter,
+			TouchFilterType = paramTable.MaintainFilterType,
+			
+			RawResults = paramTable.RawResults,
+			
+			ParamTable = paramTable
+			
+		}, TouchParams);
 
-	return self;
-end
+		return self;
+	end
 
 
 
-return TouchParams;
+	return TouchParams;
+
+end)
