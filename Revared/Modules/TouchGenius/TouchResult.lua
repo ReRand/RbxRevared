b3cbc05e366c413d20820696ba1b3bc484f7a195
shysolocup <88659700+shysolocup@users.noreply.github.com>
2024-06-30 10:35:40 -0500

    Added TouchGenius module

---
+++ Revared/Modules/TouchGenius/TouchResult.lua
@@ -0,0 +1,65 @@
+local TouchResult = {};
+TouchResult.__index = TouchResult;
+
+
+
+function TouchResult.new(touchGenius, hit)
+	local part = touchGenius.Part;
+	
+	
+	local surfaces = {
+		[Enum.NormalId.Back] = part.CFrame * CFrame.new(0, 0, part.Size.Z);
+		[Enum.NormalId.Front] = part.CFrame * CFrame.new(0, 0, -part.Size.Z);
+		[Enum.NormalId.Top] = part.CFrame * CFrame.new(0, part.Size.Y, 0);
+		[Enum.NormalId.Bottom] = part.CFrame * CFrame.new(0, -part.Size.Y, 0);
+		[Enum.NormalId.Right] = part.CFrame * CFrame.new(part.Size.X, 0, 0);
+		[Enum.NormalId.Left] = part.CFrame * CFrame.new(-part.Size.X, 0, 0);
+	}
+	
+	local surface = Enum.NormalId.Front;
+	
+	
+	for side, cframe in pairs(surfaces) do
+		surface = ((hit.Position - cframe.p).Magnitude > (hit.Position - surfaces[surface].p).Magnitude and surface or side)
+	end
+	
+	local distance = (part.Position - hit.Position).Magnitude
+	
+	
+	local self = setmetatable({
+		Instance = hit,
+		Distance = distance,
+		
+		Position = hit.Position,
+		Rotation = hit.Rotation,
+		CFrame = hit.CFrame,
+		
+		SideTouched = surface,
+		Normal = nil,
+		Material = hit.Material
+		
+	}, TouchResult);
+	
+	
+	local params = RaycastParams.new()
+	params.FilterDescendantsInstances = { hit, part };
+	params.FilterType = Enum.RaycastFilterType.Include;
+
+
+	local direction = (hit.Position - part.Position).Unit;
+	local result = workspace:Raycast(part.Position, direction*10, params)
+
+
+	if result then
+		self.Distance = result.Distance;
+		self.Normal = result.Normal;
+	end
+	
+	
+	return self;
+end
+
+
+
+
+return TouchResult;
